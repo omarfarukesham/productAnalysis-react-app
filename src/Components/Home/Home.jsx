18165c34445a52bffa73e44526ff46b4abc9fd91
Omar Faruk <omarfarukesham@gmail.com>
2022-04-04 00:52:55 +0600

    banner section has done successfully

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
-
+import React from "react";
+import image from "../offer2.jpg";
+import "./Home.css";
 const Home = () => {
-    return (
-        <div>
-            <h1>This is Home pages</h1>
-        </div>
-    );
+  return (
+    <div className=" d-flex justify-content-center align-items-center">
+      <div className="banner--design">
+        <img className="" src={image} class=" " alt="..." />
+      </div>
+      <div className="text--design">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ad
+          recusandae harum ullam? Autem voluptas quae, dignissimos et aut vel
+          sequi fugiat natus quaerat laboriosam deleniti libero veritatis in,
+          nihil quos dolorem. Ipsa velit ratione a dicta. Animi sint, magni
+          officia numquam, deleniti possimus totam asperiores rem, non tempore
+          eligendi?
+        </p>
+      </div>
+    <hr></hr>
+    
+
+    </div>
+  );
 };
 
 export default Home;
