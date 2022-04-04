3798eb7b8043667d623147f7f31f50529b3bba98
Omar Faruk <omarfarukesham@gmail.com>
2022-04-04 15:35:05 +0600

    dashboard chart integreted done

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
-    return (
-        <div>
-            this is dashboard...................
+    const[charts, setCharts] = useState([])
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=>res.json())
+        .then(data => setCharts(data))
+
+    },[])
+   
+console.log(charts);
+    return ( 
+        <div className='d-flex justify-content-center align-items-center gap-4'>
+        
+         <div className='m-3'>
+             <h3 className='text-primary'>First Quarter</h3>
+            <LineChart width={400} height={400} data={charts}>
+                <Line  dataKey={"sell"} stroke="#8884d8" />
+                <XAxis dataKey="month" />
+                <YAxis />
+                <Tooltip></Tooltip>
+            </LineChart>
+         </div>
+
+         <div className='m-2'>
+            <h3 className='text-primary'>Second Quarter</h3>
+            <BarChart width={400} height={400} data={charts}>
+                <XAxis dataKey="month"  />
+                <YAxis />
+                <Bar dataKey="revenue" barSize={25} fill="#8884d8" />
+                <Tooltip></Tooltip>
+             </BarChart>
+         </div>
+        
         </div>
+        
     );
 };
 
